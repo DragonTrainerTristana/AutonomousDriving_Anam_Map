6ea81b53f2e02d0dbe8969fcc4c32ca7486b9bd7
Seung Hyun OH <48664307+DragonTrainerTristana@users.noreply.github.com>
2023-06-28 02:16:38 +0900

    navmesh
    Yes~

--- Assets/Script/Object.cs
+++ Assets/Script/Object.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;   // 스크립트에서 내비게이션 시스템 기능을 사용하려면 AI 네임스페이스를 using 선언해야함
+using static UnityEditor.PlayerSettings;
 
 public class Object : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // 길을 찾아서 이동할 에이전트
+    NavMeshAgent agent;
+
+    // 에이전트의 목적지
+    [SerializeField]
+    Transform target;
+
+    private void Awake()
     {
-        
+
+     
+        // 게임이 시작되면 게임 오브젝트에 부착된 NavMeshAgent 컴포넌트를 가져와서 저장
+        agent = GetComponent<NavMeshAgent>();
+        agent.enabled = false; //네비끄기
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+    }
+
     void Update()
     {
-        
+        // 스페이스 키를 누르면 Target의 위치까지 이동하는 경로를 계산해서 이동
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // 에이전트에게 목적지를 알려주는 함수
+            agent.SetDestination(target.position);
+            agent.enabled = true;
+        }
     }
 }
