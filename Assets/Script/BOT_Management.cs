73b4f9a940d9b43f4564485ff72c3df3a8806ec6
Seung Hyun OH <48664307+DragonTrainerTristana@users.noreply.github.com>
2023-06-30 16:10:11 +0900

    modification

--- Assets/Script/BOT_Management.cs
+++ Assets/Script/BOT_Management.cs
@@ -11,28 +11,41 @@ public class BOT_Management : MonoBehaviour
     float min, max;
     float randomPos1, randomPos2;
 
+    Vector3 [] ranPos;
     // Random range of bot position
 
 
     [SerializeField]
     private GameObject[] botArray;
-    Vector3 ranPos;
-
+      
     void Start()
     {
         min = -15.0f; max = 15.0f;
 
         botArray = new GameObject[botNum];
+        ranPos = new Vector3[botNum];
+
         for (int i = 0; i < botNum; i++) {
-            Debug.Log(i);
             randomPos1 = Random.Range(min, max);
             randomPos2 = Random.Range(min, max);
 
-            ranPos.x = randomPos1;
-            ranPos.y = 1.0f;
-            ranPos.z = randomPos2;
+            // 같은 좌표일 때
+            if (i >= 1) {
+                for (int j = 0; j < i; j++) {
+                    if (ranPos[j].x == ranPos[i].x && ranPos[j].z == ranPos[i].z) {
+                        randomPos1 = Random.Range(min, max);
+                        randomPos2 = Random.Range(min, max);
+                        j = 0; // 처음부터 다시 확인
+                    }
+                }
+            }
 
-            botArray[i] = Instantiate(prefabBot,ranPos,Quaternion.identity);
+            ranPos[i].x = randomPos1;
+            ranPos[i].y = 1.0f;
+            ranPos[i].z = randomPos2;
+
+            botArray[i] = Instantiate(prefabBot,ranPos[i],Quaternion.identity);
+
         }
 
 
--- Assets/Script/Object.cs
+++ Assets/Script/Object.cs
@@ -7,31 +7,100 @@ public class Object : MonoBehaviour
     // 길을 찾아서 이동할 에이전트
     NavMeshAgent agent;
 
+    public GameObject[] target_Object;
+    public Transform[] target;
+
+    float originalTime = 0.0f;
+    float boundaryTime = 10.0f;
+    float stayTime = 0.0f;
+
+    bool stopTime = false;
+    float randomTime = 0.0f;
+
+    int ranNum;
+    int newNum;
+
     // 에이전트의 목적지
-    [SerializeField]
-    Transform target;
+
+
 
     private void Awake()
-    {
-
-     
-        // 게임이 시작되면 게임 오브젝트에 부착된 NavMeshAgent 컴포넌트를 가져와서 저장
+    {   
         agent = GetComponent<NavMeshAgent>();
         agent.enabled = false; //네비끄기
     }
 
     void Start()
     {
+
+
+
+        ranNum = Random.Range(0, 20);
+
+
+        // target location 
+        target_Object = new GameObject[10];
+        target = new Transform[10];
+
+
+
+        for (int i = 0; i < 10; i++) {
+            Debug.Log("TARGET (" + i + ")");
+            target_Object[i] = GameObject.Find("TARGET (" + i + ")");
+            target[i] = target_Object[i].transform;
+
+        }
+
+
+
+
     }
 
     void Update()
     {
-        // 스페이스 키를 누르면 Target의 위치까지 이동하는 경로를 계산해서 이동
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            // 에이전트에게 목적지를 알려주는 함수
-            agent.SetDestination(target.position);
-            agent.enabled = true;
+
+        originalTime += Time.deltaTime;
+
+        if (originalTime >= 5.0f) {
+            agent.speed = Random.Range(3.5f, 6.5f);
+        }
+
+        if (boundaryTime >= originalTime) {
+            originalTime = 0.0f;
+            boundaryTime = Random.Range(10.0f, 50.0f);
+            randomTime = Random.Range(5.0f, 15.0f);
+            stopTime = true;
+        }
+
+        if (stopTime == true) {
+            stayTime += Time.deltaTime;
+
+            agent.speed = 0.0f;
+
+            if (stayTime >= randomTime) {
+                stopTime = false;
+                stayTime = 0.0f;
+                agent.speed = 5.0f;
+            }
+        }
+
+        agent.SetDestination(target[ranNum].position);
+        agent.enabled = true;
+       
+
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "TARGET") {
+
+            for (;;) {
+                newNum = Random.Range(0, 10);
+                if (newNum == ranNum) { 
+                }
+                if (newNum != ranNum) { ranNum = newNum;  break; }
+            }
         }
     }
 }
